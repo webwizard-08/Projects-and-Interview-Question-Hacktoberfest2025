5fe72f71d72e2cfc2db6ae2aefde3953dd63f36b
Arnav Parmar <parmararnav23@gmail.com>
2025-10-03 19:36:34 +0530

    added linear and binary search

---
+++ C INTERVIEW/searching/binarysearch.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid; 
+        else if (arr[mid] < key)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1; 
+}
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 40;
+
+    int result = binarySearch(arr, n, key);
+    if (result != -1)
+        printf("Element %d found at index %d\n", key, result);
+    else
+        printf("Element %d not found\n", key);
+
+    return 0;
+}
+
+
