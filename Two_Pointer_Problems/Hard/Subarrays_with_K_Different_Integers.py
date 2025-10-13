bea61515a63278fa626bb11162dd5b99f3bcbd32
Aditi Sonkar <imt_2023004@iiitm.ac.in>
2025-10-13 14:58:18 +0530

    Added two pointers code

---
+++ Two_Pointer_Problems/Hard/Subarrays_with_K_Different_Integers.py
@@ -0,0 +1,45 @@
+#Problem Statement:
+#Given an integer array nums and an integer k, return the number of good subarrays of nums. A good array is an array where the number of different integers in that array is exactly k.
+#For example, [1,2,3,1,2] has 3 different integers: 1, 2, and 3.
+#A subarray is a contiguous part of an array.
+
+#Example 1:
+#Input: nums = [1,2,1,2,3], k = 2
+#Output: 7
+#Explanation: Subarrays formed with exactly 2 different integers: [1,2], [2,1], [1,2], [2,3], [1,2,1], [2,1,2], [1,2,1,2]
+
+#Example 2:
+#Input: nums = [1,2,1,3,4], k = 3
+#Output: 3
+#Explanation: Subarrays formed with exactly 3 different integers: [1,2,1,3], [2,1,3], [1,3,4].
+
+#Constraints:
+#1 <= nums.length <= 2 * 104
+#1 <= nums[i], k <= nums.length
+
+#Code:
+
+class Solution:
+    def countSubarrays(self, nums, k):
+        from collections import defaultdict
+        
+        n = len(nums)
+        mpp = defaultdict(int)
+        l = 0
+        cnt = 0
+        
+        for r in range(n):
+            mpp[nums[r]] += 1
+            
+            while len(mpp) > k:
+                mpp[nums[l]] -= 1
+                if mpp[nums[l]] == 0:
+                    del mpp[nums[l]]
+                l += 1
+            
+            cnt += r - l + 1
+        
+        return cnt
+
+    def subarraysWithKDistinct(self, nums, k):
+        return self.countSubarrays(nums, k) - self.countSubarrays(nums, k - 1)
