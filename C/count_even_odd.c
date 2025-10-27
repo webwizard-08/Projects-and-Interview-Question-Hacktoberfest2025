6ed22eb9762a6ee4a90d6679243f63fa95365de0
Kusumitha24 <konkalakusumitha1628@gmail.com>
2025-10-27 12:00:36 +0530

    Added basic array programs in C for Hacktoberfest 2025

--- C/count_even_odd.c
+++ C/count_even_odd.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
-    int n, i;
-    int even = 0, odd = 0;
+int main() {
+    int arr[10], n, i, even = 0, odd = 0;
 
-    // Input array size
-    printf("Enter the number of elements: ");
+    printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    int arr[n];
-
-    // Input array elements
     printf("Enter %d elements:\n", n);
-    for (i = 0; i < n; i++)
-    {
-        scanf("%d", &arr[i]);
-    }
+    for (i = 0; i < n; i++) scanf("%d", &arr[i]);
 
-    // Count even and odd numbers
-    for (i = 0; i < n; i++)
-    {
+    for (i = 0; i < n; i++) {
         if (arr[i] % 2 == 0)
             even++;
         else
             odd++;
     }
 
-    // Display results
-    printf("Number of even elements = %d\n", even);
-    printf("Number of odd elements = %d\n", odd);
-
+    printf("Even elements: %d\n", even);
+    printf("Odd elements: %d\n", odd);
     return 0;
 }
--- C/max_min_array.c
+++ C/max_min_array.c
@@ -1,38 +1,22 @@
 #include <stdio.h>
 
-int main()
-{
-    int n, i;
-    int max, min;
+int main() {
+    int n, i, arr[50], max, min;
 
-    // Input number of elements
-    printf("Enter the number of elements: ");
+    printf("Enter number of elements: ");
     scanf("%d", &n);
 
-    int arr[n];
-
-    // Input array elements
     printf("Enter %d elements:\n", n);
     for (i = 0; i < n; i++)
-    {
         scanf("%d", &arr[i]);
-    }
 
-    // Initialize max and min
     max = min = arr[0];
-
-    // Find max and min
-    for (i = 1; i < n; i++)
-    {
-        if (arr[i] > max)
-            max = arr[i];
-        if (arr[i] < min)
-            min = arr[i];
+    for (i = 1; i < n; i++) {
+        if (arr[i] > max) max = arr[i];
+        if (arr[i] < min) min = arr[i];
     }
 
-    // Display results
-    printf("Maximum element = %d\n", max);
-    printf("Minimum element = %d\n", min);
-
+    printf("Maximum element: %d\n", max);
+    printf("Minimum element: %d\n", min);
     return 0;
 }
