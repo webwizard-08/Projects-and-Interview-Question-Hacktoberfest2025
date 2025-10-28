696f55abd93e2de35e887292083f5430bb4126bc
Ayushvishwakarma04 <ayushvishwakarma9625@gmail.com>
2025-10-28 19:04:12 +0530

    Add sorting algorithms implementations and Sql interview questions

---
+++ C++ INTERVIEW/Algorithms/Sorting/bubble_sort.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+
+void bubbleSort(std::vector<int>& arr) {
+    int n = arr.size();
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                std::swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main() {
+    std::vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
+    bubbleSort(arr);
+    for (int x : arr) std::cout << x << " ";
+    return 0;
+}
---
+++ C++ INTERVIEW/Algorithms/Sorting/merge_sort.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+void merge(std::vector<int>& arr, int l, int m, int r) {
+    std::vector<int> left(arr.begin() + l, arr.begin() + m + 1);
+    std::vector<int> right(arr.begin() + m + 1, arr.begin() + r + 1);
+    int i = 0, j = 0, k = l;
+    while (i < left.size() && j < right.size()) {
+        if (left[i] <= right[j]) {
+            arr[k++] = left[i++];
+        } else {
+            arr[k++] = right[j++];
+        }
+    }
+    while (i < left.size()) arr[k++] = left[i++];
+    while (j < right.size()) arr[k++] = right[j++];
+}
+
+void mergeSort(std::vector<int>& arr, int l, int r) {
+    if (l < r) {
+        int m = l + (r - l) / 2;
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+        merge(arr, l, m, r);
+    }
+}
+
+int main() {
+    std::vector<int> arr = {64, 34, 25, 12, 22, 11, 90};
+    mergeSort(arr, 0, arr.size() - 1);
+    for (int x : arr) std::cout << x << " ";
+    return 0;
+}
