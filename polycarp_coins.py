3c9137b6f0085949c06e7444b3082ffc2ec2086f
ManshaAgarwal716 <manshagr716@gmail.com>
2025-10-29 22:08:20 +0530

    Add solution for Sakurako's Exam problem

--- polycarp_coins.py
+++ polycarp_coins.py
@@ -1,12 +1,26 @@
-t=int(input())
-for i in range(t):
-    n=int(input())
-    a=n//3
-    b=n%3
-    if b!=0:
-        if b>=2:
-            print(a+(b%2),a+(b//2))
-        else:
-            print(a+1,a)
-    else:
-        print(a,a)
+def solve_polycarp_coins(n):
+    """
+    Solve the Polycarp coins distribution problem.
+    
+    Args:
+        n (int): Total sum to be distributed using coins of value 1 and 2
+    
+    Returns:
+        tuple: Number of coins of value 1 and 2 respectively
+    """
+    a = n // 3  # Base distribution
+    b = n % 3   # Remainder to be distributed
+    
+    if b == 0:
+        return (a, a)
+    elif b == 1:
+        return (a + 1, a)
+    else:  # b == 2
+        return (a + b % 2, a + b // 2)
+
+# Process multiple test cases
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    c1, c2 = solve_polycarp_coins(n)
+    print(c1, c2)
